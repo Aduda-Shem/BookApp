4ffb5c168d8dff54d549e93e34084c969acfb4d3
pwnresist <shemaduida@gmail.com>
2024-06-10 02:33:00 +0300

    added images to show the application frontend

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,13 @@
+// src/components/Header.js
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import logo from '../logo.jpg';
 
 const Header: React.FC = () => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
+        <Box component="img" src={logo} alt="Kids Reading App Logo" sx={{ height: 40, marginRight: 2 }} />
         <Typography variant="h6" sx={{ flexGrow: 1, fontFamily: 'Mulish, Arial, sans-serif' }}>
           Ello Books
         </Typography>
